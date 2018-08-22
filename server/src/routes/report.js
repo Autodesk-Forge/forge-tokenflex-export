4fab9ce44ddfddb99017739217214cdd24b60ce7
Bastien Mazeran <15131317+mazerab@users.noreply.github.com>
2018-08-22 11:23:36 -0700

    contract implementation

--- server/src/routes/report.js
+++ server/src/routes/report.js
@@ -7,6 +7,25 @@ const config = require('./../configuration/config.json')
 const router = new Router({ prefix: '/api' })
 
 router.get(
+  '/reports/contracts',
+  async (ctx) => {
+    if (ctx.isAuthenticated) {
+      const options = {
+        agentOptions: { secureProtocol: config.tlsversion },
+        headers: { Authorization: `Bearer ${ctx.session.publicCredentials.access_token}` },
+        json: true,
+        method: 'GET',
+        url: 'https://developer-stg.api.autodesk.com/eccr-data-api/v1/contract'
+      }
+      const response = await rp(options)
+      ctx.body = JSON.stringify(response)
+    } else {
+      ctx.throw(401)
+    }
+  }
+)
+
+router.get(
   '/user/profile',
   async (ctx) => {
     if (ctx.isAuthenticated) {
